2a865e6af55ba4936b5acddf5b902442000e9ba2
ex-Plant <konradantonik@gmail.com>
2025-11-15 21:09:22 +0100

    remove not existing post hero

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -10,7 +10,7 @@ import RichText from '@/components/RichText'
 
 import type { Post } from '@/payload-types'
 
-import { PostHero } from '@/heros/PostHero'
+// import { PostHero } from '@/heros/PostHero'
 import { generateMeta } from '@/utilities/generateMeta'
 import PageClient from './page.client'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
@@ -60,7 +60,7 @@ export default async function Post({ params: paramsPromise }: Args) {
 
       {draft && <LivePreviewListener />}
 
-      <PostHero post={post} />
+      {/* <PostHero post={post} /> */}
 
       <div className="flex flex-col items-center gap-4 pt-8">
         <div className="container">
