1db24acea0b77c62be3a4e5ad1e806aa6d0a0c9f
ex-Plant <konradantonik@gmail.com>
2025-12-19 13:13:25 +0100

    fix build errors

--- src/app/(frontend)/post-purchase/[token]/page.tsx
+++ src/app/(frontend)/post-purchase/[token]/page.tsx
@@ -1,16 +1,9 @@
 // import { notFound } from 'next/navigation'
-import { getPayload } from 'payload'
-import configPromise from '@payload-config'
-
-type PostPurchasePagePropsT = {
-  params: { token: string }
-}
 
 export const dynamic = 'force-dynamic'
 
-export default async function PostPurchasePage({ params }: PostPurchasePagePropsT) {
+export default async function PostPurchasePage() {
   //   const token: string = params.token
-  console.log(params.token)
 
   //   const payload = await getPayload({ config: configPromise })
 
