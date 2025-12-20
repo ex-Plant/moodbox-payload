5779a810096dddd6ee5862dced9defea87777545
ex-Plant <konradantonik@gmail.com>
2025-12-20 11:53:19 +0100

    fix build errors

--- scratch.ts
+++
@@ -1,9 +0,0 @@
-import { createHmac } from 'crypto'
-
-const orderId = 'orderidsialalal'
-const secret = '123123123'
-
-const hmac = createHmac('sha256', secret).update(orderId).digest('hex')
-
-
-const decoded = 
