22e3437e18912c73775c73832613d89223de6f7f
ex-Plant <konradantonik@gmail.com>
2025-12-22 12:04:25 +0100

    send emails with debug logs to test cron

--- src/app/api/cron/send-feedback-emails/route.ts
+++ src/app/api/cron/send-feedback-emails/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
+import configPromise from '@payload-config'
 import { sendScheduledEmail } from '@/lib/shopify/webhooks/sendScheduledEmail'
+import { getPayload } from 'payload'
 
 export const maxDuration = 300
 export const dynamic = 'force-dynamic'
@@ -13,9 +15,33 @@ export async function POST(req: NextRequest) {
   const authHeader: string | null = req.headers.get('authorization')
   const expected: string = `Bearer ${process.env.CRON_SECRET ?? ''}`
 
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const cronExecutionTime = new Date().toISOString()
+
   if (!process.env.CRON_SECRET || authHeader !== expected) {
+    const text = `authHeader: ${authHeader}, CRON_SECRET: ${process.env.CRON_SECRET}`
+
+    // TODO REMOVE AFTER TESTING
+    await payload.sendEmail({
+      to: process.env.EMAIL_USER || '',
+      subject: `CRON TEST EMAIL FAILED`,
+      text: text,
+      html: `<p>Cron execution time: ${cronExecutionTime}</p>`,
+    })
+
     return new NextResponse('Unauthorized', { status: 401 })
   }
+
+  // TODO REMOVE AFTER TESTING
+  await payload.sendEmail({
+    to: process.env.EMAIL_USER || '',
+    subject: `CRON TEST EMAIL `,
+    text: `SENDING FEEDBACK EMAILS`,
+    html: `<p>Cron execution time: ${cronExecutionTime}</p>`,
+  })
 
   try {
     const results = await sendScheduledEmail()
@@ -28,4 +54,7 @@ export async function POST(req: NextRequest) {
     const message = e instanceof Error ? e.message : '❌ Unexpected error while sending'
     return NextResponse.json({ error: message, status: 500 })
   }
+}
+function getPayloadClient(arg0: { config: any }) {
+  throw new Error('Function not implemented.')
 }
