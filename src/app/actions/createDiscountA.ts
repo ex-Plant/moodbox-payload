16674e944f9a63080d74cf8a3844ea5a87b32e8a
ex-Plant <konradantonik@gmail.com>
2026-01-08 20:25:07 +0100

    adjust creating discount logic

--- src/app/actions/createDiscountA.ts
+++ src/app/actions/createDiscountA.ts
@@ -2,18 +2,22 @@
 
 import { UI_MESSAGES } from '../../components/_custom_moodbox/survey/survey_constants'
 import { createDiscountCode } from '../../lib/shopify/adminApi'
+import { generateHexToken } from '../../utilities/generateTokenString'
 
-export async function createDiscountA(code: string) {
+export async function createDiscountA() {
+  const code = generateHexToken(6, 'MOODBOX')
+  // console.log('createDiscountA.ts:9 - code:', code)
+
+  // Calculate expiration date 30 days limit
+  const endsAt = new Date()
+  endsAt.setDate(endsAt.getDate() + 30)
+
   const result = await createDiscountCode({
     title: UI_MESSAGES.WELCOME_DISCOUNT_TITLE,
     code: code,
+    endsAt: endsAt.toISOString(), // Shopify expects ISO8601 string
     usageLimit: 1,
     appliesOncePerCustomer: true,
-    // minimumRequirement: {
-    //   subtotal: {
-    //     greaterThanOrEqualToSubtotal: '50.00',
-    //   },
-    // },
     customerGets: {
       value: {
         percentage: 0.1,
@@ -24,7 +28,7 @@ export async function createDiscountA(code: string) {
     },
   })
 
-  console.log(result)
+  console.log('createDiscountCode: ', result)
 
   if (result.success) {
     console.log(UI_MESSAGES.DISCOUNT_SUCCESS_MESSAGE, result.discountId)
