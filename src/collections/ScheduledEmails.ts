4c77439887db0768204c1652211d3b9ff7fb1c72
ex-Plant <konradantonik@gmail.com>
2026-01-09 10:29:39 +0100

    Allow editing sent status manually

--- src/collections/ScheduledEmails.ts
+++ src/collections/ScheduledEmails.ts
@@ -1,9 +1,27 @@
 import type { CollectionConfig } from 'payload'
+import { authenticated } from '../access/authenticated'
 
 export const ScheduledEmails: CollectionConfig = {
   slug: 'scheduled-emails',
+  labels: {
+    singular: {
+      pl: 'Zaplanowana Wiadomość',
+      en: 'Scheduled Email',
+    },
+    plural: {
+      pl: 'Zaplanowane Wiadomości',
+      en: 'Scheduled Emails',
+    },
+  },
+  access: {
+    create: authenticated,
+    delete: authenticated,
+    read: authenticated,
+    update: authenticated,
+  },
   admin: {
     useAsTitle: 'orderId',
+    defaultColumns: ['orderId', 'customerEmail', 'status', 'scheduledAt'],
     components: {
       afterListTable: ['@/components/TriggerSendingScheduledEmails'],
     },
@@ -13,21 +31,33 @@ export const ScheduledEmails: CollectionConfig = {
       name: 'orderId',
       type: 'text',
       required: true,
+      admin: {
+        readOnly: true,
+      },
     },
     {
       name: 'customerEmail',
       type: 'email',
       required: true,
+      admin: {
+        readOnly: true,
+      },
     },
     {
       name: 'scheduledAt',
       type: 'date',
       required: true,
+      admin: {
+        readOnly: true,
+      },
     },
     {
       name: 'expiresAt',
       type: 'date',
       required: true,
+      admin: {
+        readOnly: true,
+      },
     },
     {
       name: 'status',
@@ -46,6 +76,9 @@ export const ScheduledEmails: CollectionConfig = {
       type: 'select',
       required: true,
       defaultValue: 'post_purchase_questions',
+      admin: {
+        readOnly: true,
+      },
       options: [{ label: 'Post purchase questions', value: 'post_purchase_questions' }],
     },
     {
@@ -53,11 +86,17 @@ export const ScheduledEmails: CollectionConfig = {
       type: 'text',
       required: true,
       unique: true,
+      admin: {
+        readOnly: true,
+      },
     },
     {
       name: 'isSurveyCompleted',
       type: 'checkbox',
       defaultValue: false,
+      admin: {
+        readOnly: true,
+      },
     },
   ],
 }
