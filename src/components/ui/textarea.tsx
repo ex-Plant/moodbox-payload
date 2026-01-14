6a22050f30440c055b268a97f7edf8cc56d760eb
ex-Plant <konradantonik@gmail.com>
2026-01-14 14:11:50 +0100

    adjust text area styling

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea: React.FC<
   return (
     <textarea
       className={cn(
-        'flex min-h-[80px] w-full  bg-white px-6 py-4  placeholder:text-muted-foreground focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-ring  aria-invalid:ring-destructive/20 aria-invalid:border-destructive !aria-invalid:border-destructive  aria-invalid:text-destructive rounded-md border text-base shadow-xs  transition-[color,box-shadow] outline-none   ',
+        'flex min-h-[80px] w-full  bg-white px-6 py-4  placeholder:text-muted-foreground focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-ring  aria-invalid:ring-destructive/20 aria-invalid:border-destructive !aria-invalid:border-destructive  aria-invalid:text-destructive rounded-md border text-base shadow-xs  transition-[color,box-shadow] outline-none    ',
         className,
       )}
       ref={ref}
